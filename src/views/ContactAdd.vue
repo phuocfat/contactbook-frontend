e8b047c23d01c1bd7629945cfb4eed39080e4e64
phuocfat <72081997+phuocfat@users.noreply.github.com>
2022-04-04 14:10:50 +0700

    Tao trang them lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page">
+    <h4>Thêm Liên hệ</h4>
+    <ContactForm :contact="contact" @submit:contact="createContact"/>
+    <p>{{ message }}</p>
+  </div>
+</template> 
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+  components: {
+    ContactForm,
+  },
+  data() {
+    return { 
+      contact: {
+        type: Object,
+        require: true
+      }, 
+      message: ""
+    }
+  },
+  methods: {
+  async createContact(data) {
+    try {
+
+      await ContactService.create(data);
+      this.$router.push({ name: "contactbook" });
+    } catch(error) {
+      console.log(error);
+    }
+    },
+  },
+};
+</script>
